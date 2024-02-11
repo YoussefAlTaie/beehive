fdd7f5f8091c3be99c124ed4330c242ad8df97b1
YoussefAlTaie <155756367+YoussefAlTaie@users.noreply.github.com>
2024-02-11 20:42:48 +0300

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,12 @@
-const express = require("express")
-const app = express();
-var listener = app.listen(process.env.PORT || 2000, function () {
-  console.log('Your app is listening on port ' + listener.address().port);
-});
-app.listen(() => console.log("I'm Ready To Work..! 24H"));
-app.get('/', (req, res) => {
-  res.send(`
-  <body>
-  <center><h1>Bot 24H ON!</h1></center
-  </body>`)
-});
-const { Client } = require('discord.js-selfbot-v13');
-const client = new Client(); // All partials are loaded automatically
+const Discord = require("discord.js-selfbot-v13")
+const sphinx = require("sphinx-self");
+const keep_alive = require("./keep_alive.js");
+const tokens = process.env["token"].split("**")
+const clients = [];
+for (const token of tokens) {
+  
+const client = new Discord.Client({checkUpdate: false})
+new sphinx.Core(client).leveling({ channel: "1187808730440540294", randomLetters: true, type: 'ar', time: 180000 }) //hover for options
 
-client.on('ready', async () => {
-  console.log(`${client.user.username} is ready!`);
-})
-//ثبات فويس 24 ساعه v13 بدون اي مشاكل
-const { joinVoiceChannel } = require('@discordjs/voice');
-client.on('ready', () => {
-
-    setInterval( async () => {
-    client.channels.fetch(process.env.channel) 
-     .then((channel) => { 
-      const VoiceConnection = joinVoiceChannel({
-       channelId: channel.id, 
-       selfDeaf: true,
-       selfMute: true,
-       guildId: channel.guild.id, 
-       adapterCreator: channel.guild.voiceAdapterCreator 
-       });
-    }).catch((error) => { return; });
-    }, 1000)
-}); 
-//https://ra3dstudio.com CopyRight Codes
-client.login(process.env.token);
+client.login(token) //Not saved.
+}  
