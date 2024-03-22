1cafe1a70b30b9044c57c2b501060bebda244949
YoussefAlTaie <155756367+YoussefAlTaie@users.noreply.github.com>
2024-03-22 15:55:35 -0700

    Create index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+const Discord = require("discord.js-selfbot-v13")
+const sphinx = require("sphinx-self");
+const keep_alive = require("./keep_alive.js");
+const tokens = process.env["token"].split("**")
+const clients = [];
+for (const token of tokens) {
+  
+const client = new Discord.Client({checkUpdate: false})
+new sphinx.Core(client).leveling({ channel: "1220830635422253220", randomLetters: true, type: 'ar', time: 420000 }) //hover for options
+
+client.login(token) //Not saved.
+}  
