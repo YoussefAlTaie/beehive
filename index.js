5b3b8a9f4cc227a8b900e7026190501ef1099e9b
YoussefAlTaie <155756367+YoussefAlTaie@users.noreply.github.com>
2024-03-28 22:58:40 +0300

    Update index.js

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ const clients = [];
 for (const token of tokens) {
   
 const client = new Discord.Client({checkUpdate: false})
-new sphinx.Core(client).leveling({ channel: "1220830635422253220", randomLetters: true, type: 'ar', time: 420000 }) //hover for options
+new sphinx.Core(client).leveling({ channel: "1220830635422253220", randomLetters: true, type: 'ar', time: 360000 }) //hover for options
 
 client.login(token) //Not saved.
 }  
